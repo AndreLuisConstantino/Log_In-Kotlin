9e9e890635e22cd9f778386f590a0ed3bd8fb382
ANDRÉ LUIZ CONSTANTINO <22282216@dsn127m1246730.cfp127aluno.local>
2023-03-21 16:54:59 -0300

    Inicio do Home

---
+++ app/src/main/java/br/senai/sp/jandira/login_signup/Home.kt
@@ -0,0 +1,72 @@
+package br.senai.sp.jandira.login_signup
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.senai.sp.jandira.login_signup.ui.theme.LoginSignupTheme
+
+class Home : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LoginSignupTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    homeScreen()
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun homeScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(206.dp),
+            Alignment.TopEnd
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.image_paris),
+                contentDescription = "",
+                modifier = Modifier
+                    .fillMaxSize()
+            )
+            Image(
+                painter = painterResource(id = R.drawable.susanna_profile),
+                contentDescription = "",
+                modifier = Modifier
+                    .size(61.dp)
+                    .clip(shape = CircleShape)
+                    .border(
+                        width = 2.dp,
+                        shape = CircleShape,
+                        color = Color(255, 255, 255, 255)
+                    )
+
+            )
+        }
+    }
+}
